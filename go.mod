3b7a5181eeca7266be006f99de7ab28f55cd50d9
wallacez11 <wallace.santos.mail@gmail.com>
2023-10-26 19:30:08 -0300

    criando primeiro endpoint

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/wallacez11/go-rssaggregator
 
 go 1.21.1
+
+require (
+	github.com/go-chi/cors v1.2.1
+	github.com/joho/godotenv v1.5.1
+)
+
+require github.com/go-chi/chi v1.5.5
--- main.go
+++ main.go
@@ -1,8 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
 
-	fmt.Println("hello world")
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Could not load env variable")
+	}
+
+	v1Router := chi.NewRouter()
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: true,
+		ExposedHeaders:   []string{"*"},
+		MaxAge:           300,
+	}))
+
+	v1Router.HandleFunc("/ready", handlerReadiness)
+	router.Mount("/v1", v1Router)
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+
+	err := srv.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Port: ", portString)
 }
